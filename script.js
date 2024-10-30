01bb14db65f29e0b4afae24c9acd76cc7dfae2e9
viblo29 <dachivibliani29@gmail.com>
2024-10-30 22:40:24 +0400

    1st

---
+++ script.js
@@ -0,0 +1,15 @@
+let firstName = "Dachi";
+let lastName = "Vibliani";
+let age = 100;
+let email = "dachivibliani29@gmail.com";
+let phoneNumber = 5515902;
+let isAdult = true;
+
+console.log("First Name:", firstName);
+console.log("Last Name:", lastName);
+console.log("Age:", age);
+console.log("Email:", email);
+
+console.log("%cPhone Number: ", "color: red;", phoneNumber);
+
+console.log("%cIs Adult:", "color: yellow;", isAdult);
