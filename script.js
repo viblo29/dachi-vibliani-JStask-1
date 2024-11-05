ac3228611db5261dfafd94ed22f18ec8570de7e5
viblo29 <dachivibliani29@gmail.com>
2024-11-05 15:47:51 +0400

    JS Task 2

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
-let firstName = "Dachi";
-let lastName = "Vibliani";
-let age = 100;
-let email = "dachivibliani29@gmail.com";
-let phoneNumber = 5515902;
-let isAdult = true;
+// let firstName = "Dachi";
+// let lastName = "Vibliani";
+// let age = 100;
+// let email = "dachivibliani29@gmail.com";
+// let phoneNumber = 5515902;
+// let isAdult = true;
 
-console.log("First Name:", firstName);
-console.log("Last Name:", lastName);
-console.log("Age:", age);
-console.log("Email:", email);
+// console.log("First Name:", firstName);
+// console.log("Last Name:", lastName);
+// console.log("Age:", age);
+// console.log("Email:", email);
 
-console.log("%cPhone Number: ", "color: red;", phoneNumber);
+// console.log("%cPhone Number: ", "color: red;", phoneNumber);
 
-console.log("%cIs Adult:", "color: yellow;", isAdult);
+// console.log("%cIs Adult:", "color: yellow;", isAdult);
+
+
+let Num = 11;
+let Square = Num * Num;
+
+if (Square > 100) {
+    console.log("მეტია 100-ზე")
+} else {
+    console.log("ნაკლებია 100-ზე")
+}
+
+let Num1 = 10;
+
+if (Num1 > 50 && Num1 < 100) {
+    console.log("ეს რიცხვი არის 50-ზე მეტი და 100-ზე ნაკლები")
+} else {
+    console.log("ეს რიცხვი AR არის 50-ზე მეტი და 100-ზე ნაკლები")
+}
+
+let Num2 = -10;
+
+if (Num2 > 0) {
+    console.log("მეტია 0-ზე")
+} else if (Num2 === 0) {
+    console.log("0-ის ტოლია")
+} else if (Num2 < 0) {
+    console.log("ნაკლებია 0-ზე")
+}
